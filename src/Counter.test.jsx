be36ff2d475b6c86a7b3d9cf7c2d4b0922d791e1
Rafi Cantor <raficantor1@gmail.com>
2024-12-17 10:22:35 +0000

    remove initial count from tests

--- src/Counter.test.jsx
+++ src/Counter.test.jsx
@@ -5,14 +5,9 @@ afterEach(cleanup);
 
 describe(Counter, () => {
 
-    it("Counter displays correct initial count", () => {
-        const { getByTestId } = render(<Counter initialCount={0} />);
-        const countValue = Number(getByTestId("count").textContent);
-        expect(countValue).toEqual(0);
-    });
 
     it("Count should increment by 1 on increment button click", () => {
-        const { getByTestId, getByRole } = render(<Counter initialCount={0} />);
+        const { getByTestId, getByRole } = render(<Counter />);
         const incrementBttn = getByRole("button", {name: "Increment"});
         expect(Number(getByTestId("count").textContent)).toEqual(0);
         fireEvent.click(incrementBttn);
@@ -20,7 +15,7 @@ describe(Counter, () => {
     });
 
     it("Count should decrement by 1 on decrement button click", () => {
-        const { getByTestId, getByRole } = render(<Counter initialCount={0} />);
+        const { getByTestId, getByRole } = render(<Counter />);
         const decrementBttn = getByRole("button", {name: "Decrement"});
         expect(Number(getByTestId("count").textContent)).toEqual(0);
         fireEvent.click(decrementBttn);
